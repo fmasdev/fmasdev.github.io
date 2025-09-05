216a4841450665ab4458bb605bdceab7593a9574
Frederic Mas <fredfredfredm@yahoo.fr>
2025-09-05 14:06:37 +0200

    cms remove custom route

--- cms/src/api/experience/controllers/experience.js
+++ cms/src/api/experience/controllers/experience.js
@@ -6,21 +6,4 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-// module.exports = createCoreController('api::experience.experience');
-
-module.exports = createCoreController('api::experience.experience', ({ strapi }) => ({
-  async findByLocale(ctx) {
-    const { locale } = ctx.query;
-
-    const entry = await strapi.db.query('api::experience.experience').findOne({
-      where: { locale: locale || 'en' }, // par défaut EN
-      populate: { localizations: true, deep: true },
-    });
-
-    if (!entry) {
-      return ctx.notFound('Experience not found for this locale');
-    }
-
-    return entry;
-  },
-}));
+module.exports = createCoreController('api::experience.experience');
--- cms/src/api/experience/routes/experience.js
+++ cms/src/api/experience/routes/experience.js
@@ -6,17 +6,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-// module.exports = createCoreRouter('api::experience.experience');
-
-module.exports = {
-  routes: [
-    {
-      method: 'GET',
-      path: '/experience-by-locale',
-      handler: 'experience.findByLocale',
-      config: {
-        auth: false,
-      },
-    },
-  ],
-};
+module.exports = createCoreRouter('api::experience.experience');
