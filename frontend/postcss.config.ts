c594a594a5ceecf91f918d40731e251cf0774513
Frederic Mas <fredfredfredm@yahoo.fr>
2025-09-05 13:54:24 +0200

    frontend init

---
+++ frontend/postcss.config.ts
@@ -0,0 +1,11 @@
+// postcss.config.ts
+import tailwind from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
+
+
+export default {
+  plugins: [
+    tailwind(),
+    autoprefixer(),
+  ],
+}
---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,9 @@
+// tailwind.config.js
+
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies any
