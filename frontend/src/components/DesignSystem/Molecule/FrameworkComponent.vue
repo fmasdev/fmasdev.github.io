e8b72f044b156e19627d38f79730b4de1960238f
Frederic Mas <fredfredfredm@yahoo.fr>
2025-09-11 09:13:45 +0200

    front: remove framework component

--- frontend/src/components/DesignSystem/Molecule/FrameworkComponent.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>framework</div>
-  <FrameworkItemComponent />
-</template>
-
-<script setup lang="ts">
-import FrameworkItemComponent from '@components/DesignSystem/Atoms/FrameworkItemComponent.vue'
-</script>
