379aaf56869bd546711fb59b783c759fc9d03e02
Frederic Mas <fredfredfredm@yahoo.fr>
2025-09-29 12:49:10 +0200

    frontend: put current lang on html index

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,8 +3,21 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, onUnmounted, provide } from 'vue'
+import { onMounted, onUnmounted, provide, watch } from 'vue'
 import { updateDeviceType, currentDeviceType } from './helpers/deviceHelper'
+import { useI18n } from 'vue-i18n'
+
+const { locale } = useI18n()
+
+watch(
+  () => locale.value,
+  (lang) => {
+    if (!lang) return
+
+    document.documentElement.lang = lang
+  },
+  { immediate: true }
+)
 
 onMounted(() => {
   updateDeviceType()
